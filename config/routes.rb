51cdabaaafc4f7b9294209f04d1b48b105050912
DaikiMaekawa <citmae0229@gmail.com>
2019-07-09 09:07:53 +0000

    second-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'users/new'
-  get 'pages/index'
-  root 'pages#index'
-end
-
-Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
   root 'pages#index'
   get 'pages/help'
-end
-
-Rails.application.routes.draw do
-  get 'sessions/new'
-  root 'pages#index'
-  get 'pages/help'
+  
 
   resources :users
+  
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
